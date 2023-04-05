084ac9fe6c1da8c8c92de813408fbbfb4875f652
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-04 21:00:13 -0300

    praticando comparação + boolean

---
+++ tipo-primitivos/type-boolean.js
@@ -0,0 +1,20 @@
+// comparando valores atribuidos às variáveis
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+console.log(primeiroNumero === segundoNumero); //false
+
+const terceiroNumero = 4;
+const quartoNumero = 4;
+
+console.log(terceiroNumero === quartoNumero);  // true
+
+
+// comparando strings
+const texto1 = "Alura";
+const texto2 = "fruta";
+
+console.log(texto1 === texto2); // false
+
+

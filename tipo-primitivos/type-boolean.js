4b276d2729d439b48b51a7f24562593fc86ee164
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-04 21:09:26 -0300

    praticando operações logicas

--- tipo-primitivos/type-boolean.js
+++ tipo-primitivos/type-boolean.js
@@ -18,3 +18,21 @@ const texto2 = "fruta";
 console.log(texto1 === texto2); // false
 
 
+//operações lógicas ou booleanas
+// exemplo 1
+const msg1 = true;
+
+if (msg1 == true) {
+    console.log("Mensagem #1 é verdadeira")
+} else {
+    console.log("Mensagem #1 é falsa")
+}
+
+// exemplo 2
+const msg2 = false;
+
+if (msg2 == true) {
+    console.log("Mensagem #2 é verdadeira")
+} else {
+    console.log("Mensagem #2 é falsa")
+}

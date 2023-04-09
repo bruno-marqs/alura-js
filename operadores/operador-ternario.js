7f2f9a47f0adbcce6262e90979b333deac7fd2ca
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-09 11:09:55 -0300

    ternario + correção falsy

---
+++ operadores/operador-ternario.js
@@ -0,0 +1,18 @@
+
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+// usando IF padrão
+
+if (idadeCliente >= idadeMinima) {
+    console.log("O cliente bebeu cerveja");
+} else {
+    console.log("O cliente bebeu suco");
+}
+
+// usando OPERADOR TERNARIO
+
+               // condição                      //true                      // false
+console.log(idadeCliente >= idadeMinima ? "O Cliente bebeu vodka" :  "O Cliente bebeu Toddynho") 
+
+ 

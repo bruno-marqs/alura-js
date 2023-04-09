29559746ce2283be0621da581662cd2d1cd0e0e1
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-09 10:57:28 -0300

    iniciando operadores

---
+++ operadores/comparacoes.js
@@ -0,0 +1,13 @@
+// == COMPARAÇÃO IMPLICITA
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto); // resultado: true
+
+// === COMPARAÇÃO EXPLÍTICA
+
+const number = 10;
+const text = "10";
+
+console.log(number === text); // resultado: false

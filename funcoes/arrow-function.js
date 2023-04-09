709597d7f148a705ddf69f061db2f10ff79876d7
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-09 14:38:40 -0300

    aprendendo arrow-function

---
+++ funcoes/arrow-function.js
@@ -0,0 +1,9 @@
+
+// forma padrão de declarar função
+function apresentar(nome ){
+    return `Meu nome é ${nome}`
+}
+
+// como declarar arrow-function
+const apresentaArrow = nome => `meu nome é ${nome}`;
+const soma = (num1, num2) => num1 + num2;

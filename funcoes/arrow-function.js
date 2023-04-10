cbc2bbef21ba5c27dcabafd4e9d5a3d7ee05c373
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-10 12:04:23 -0300

    exemplificando arrow-funtion

--- funcoes/arrow-function.js
+++ funcoes/arrow-function.js
@@ -7,3 +7,12 @@ function apresentar(nome ){
 // como declarar arrow-function
 const apresentaArrow = nome => `meu nome é ${nome}`;
 const soma = (num1, num2) => num1 + num2;
+
+// outro exemplo
+const somarNumerosPequenos = (number1, number2) => {
+    if (number1 > 10 || number2 > 10){
+        return "somente números de 1 a 9"
+    } else {
+        return number1 + number2
+    }
+}

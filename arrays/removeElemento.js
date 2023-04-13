7855efa2f1bf70a8f901ae736210d63024d2fa84
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-13 09:55:45 -0300

    removendo elemento array

---
+++ arrays/removeElemento.js
@@ -0,0 +1,12 @@
+ 
+ console.log("Em um array de notas, remova a nota extra")
+
+ // array iniciado
+ const notas = [10, 6, 8, 5.5, 10];
+ console.log("Array com nota extra: " + notas);
+
+ // removendo elementos com método pop (remove o último elemento)
+notas.pop()
+
+ // conferindo a alteração
+console.log("Array corrigido: " + notas);

cc123831fb1a5c41634635822a808afb8f430080
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-13 11:25:46 -0300

    alterando array com splice

---
+++ arrays/modArray.js
@@ -0,0 +1,12 @@
+
+console.log("Num lista de chamada, remova os alunos ANA e CAIO que saíram e adicione RODRIGO no lugar");
+
+// iniciando array 
+const listaDeChamada = ['Ana', 'Caio', 'Lara', 'Isabela', 'Leo'];
+console.log("A lista inicial: " + listaDeChamada);
+
+// alterando a lista de chamada com método splice
+listaDeChamada.splice(0, 2, 'Rodrigo');
+
+// conferindo a alteranção
+console.log("A nova lista é: " + listaDeChamada);

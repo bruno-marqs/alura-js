39378424d6e93869a55fc02ab150a6862956ce0b
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-13 09:52:14 -0300

    adicionando elemento array

---
+++ arrays/addElemento.js
@@ -0,0 +1,12 @@
+
+console.log("Em um array de notas, adicione uma nota")
+
+// array iniciado
+const notas = [10, 6, 8];
+console.log("Como estava: " + notas);
+
+// adicionando elementos com método push
+notas.push(7);
+
+// conferindo a alteração
+console.log("Como ficou" + notas); 

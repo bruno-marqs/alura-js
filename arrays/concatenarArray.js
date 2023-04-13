4b087fbcfa11aeb71b366d4a0f5a4d1c795cb02c
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-13 11:33:32 -0300

    concatenando arrays metodo concat

---
+++ arrays/concatenarArray.js
@@ -0,0 +1,12 @@
+
+console.log("Una duas salas em um só array");
+
+// iniciando arrays
+const sala1 = ['Ana', 'Isabela', 'Caio'];
+const sala2 = ['João', 'Marcelo', 'Jessica'];
+
+// concatenando arrays com método concat
+const salaFinal = sala1.concat(sala2);
+
+// conferindo alteração
+console.log(salaFinal);

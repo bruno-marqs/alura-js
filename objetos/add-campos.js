e54e658e23c9fc57e8037441237010ae59454016
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-17 16:02:54 -0300

    add e alterando campo de valor em objeto

---
+++ objetos/add-campos.js
@@ -0,0 +1,17 @@
+//objeto
+const cliente = {
+    nome: "andré",
+    idade: 36,
+    cpf:"45678965422",
+    email:"andre@email.com"
+};
+
+console.log(cliente);
+
+// add campo de atributo
+cliente.fone = "998866584";
+console.log(cliente);
+
+// alterando valor de campo pre-existente
+cliente.fone = "886554422"
+console.log(cliente)

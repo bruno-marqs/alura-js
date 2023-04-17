bcaada1b43c78d0b9f1a4811458868e3d4b89882
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-17 15:29:45 -0300

    iniciando estudos em objetos

---
+++ objetos/objetos.js
@@ -0,0 +1,14 @@
+
+// exeemplo de array
+const listaCPFs = ["12345678915", "65498712345", "9877654321165"];
+
+// exemplo de array simulando um objeto
+const client= ["nome", "André", "idade", 36];
+
+// exemplo de objeto em javascript
+const cliente = {
+    nome: "andré",
+    idade: 36,
+    cpf:"45678965422",
+    email:"andre@email.com"
+}

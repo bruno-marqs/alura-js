a07b019e8977224400f797ae8fece274d987d7fb
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-21 16:25:14 -0300

    criando uma classe CLiente e executando métodos

---
+++ poo/classe-cliente.js
@@ -0,0 +1,29 @@
+// criando classe Cliente
+class Cliente {
+    constructor(nome, email, cpf, saldo){
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+        this.saldo = saldo;
+    }
+
+    depositar(valor){
+        this.saldo += valor;
+    }
+
+    exibirSaldo(){
+        console.log(this.saldo)
+    }
+}
+
+// instanciando novo objeto de Classe Cliente
+const cl1 = new Cliente("Gilson", "gilson@email.com", "45688532415", 100);
+
+// imprimindo objeto
+console.log(cl1)
+
+// executando função depositar
+cl1.depositar(134);
+
+// imprimindo novo saldo
+cl1.exibirSaldo();

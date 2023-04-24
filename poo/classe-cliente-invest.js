077baf45fbe268cde6f6de5eec4fb2a2480a9540
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-24 15:06:48 -0300

    criando classe

---
+++ poo/classe-cliente-invest.js
@@ -0,0 +1,14 @@
+// importando classe-mãe
+import Cliente from "./classe-cliente.js"
+
+// criando nova classe
+class ClienteInvest extends Cliente {
+    constructor(nome, email, cpf, saldo, valorInvest){
+        super(nome, email, cpf, saldo);
+        this.valorInvest = valorInvest
+    }
+
+    investir(valor){
+        this.valorInvest = valor * 0.015
+    }
+}

e5f9251e3b9f66acd210691bb9b81d3d04477515
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-25 13:24:49 -0300

    utilizando getter para alterar valor do atributo

--- poo/index.js
+++ poo/index.js
@@ -14,7 +14,9 @@ console.log(novoCliente.nome);
 const novoClientePoupanca = new ClientePoupanca('Julia', 'julia@email.com', '85462533388', 100, 300);
 console.log(novoClientePoupanca);
 
-novoClientePoupanca.#nome = 'Maria'; // error: tentando alterar o valor de um atributo privado da Classe ClientePoupança
+// novoClientePoupanca.#nome = 'Maria'; // error: tentando alterar o valor de um atributo privado da Classe Cliente
+novoClientePoupanca.getNome = 'Maria'; // alterando atributo privado utilizando getter
+console.log(novoClientePoupanca.nome);
 
 
 // instanciando objeto ClienteInvest

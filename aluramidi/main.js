23f01bd82ac91d20ab487fedfdb17d1ffe37e38e
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-26 11:49:56 -0300

    criando laço de repetição while

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -5,3 +5,10 @@ function tocaSomPom(){
 
 // criando lista das teclas
 const listaTeclas = document.querySelectorAll('.tecla');
+
+// criando laço de repetição
+let contador = 0;
+while(contador < listaTeclas.length){
+    listaTeclas[0].onclick = tocaSomPom;
+    contador ++;
+}

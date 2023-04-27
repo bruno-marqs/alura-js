bd8f1d0799b7670406295dd39fc25b18b1736bad
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-27 15:00:48 -0300

    alterando rotina arrow function + criando funcao especifico

--- robotron/main.js
+++ robotron/main.js
@@ -4,14 +4,16 @@ const somar = document.querySelector("#somar");
 const braco = document.querySelector("#braco");
 
 // criando arrow function somar
-somar.addEventListener("click", (evento) => {
-    // transformando string em int 
-    braco.value = parseInt(braco.value) + 1
-})
+somar.addEventListener("click", () => {manipulaDados("somar")})
 
 // criando arrow function subtrair
-somar.addEventListener("click", (evento) => {
-    // transformando string em int 
-    braco.value = parseInt(braco.value) - 1
-})
+somar.addEventListener("click", (evento) => {manipulaDados("subtrair")})
 
+// criando função manipular dados
+function manipulaDados(operacao) {
+    if (operacao === "subtrair"){
+        braco.value = parseInt(braco.value) - 1
+    } else {
+        braco.value = parseInt(braco.value) + 1
+    }
+}

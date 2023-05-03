ddb7204598e6b25004f9aa4dc317a485c3d719cb
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-05-03 12:10:41 -0300

    implemntação condicional MoneyMask + comentários

--- petshop/js/app.js
+++ petshop/js/app.js
@@ -1,8 +1,23 @@
 import { valida } from './validacao.js';
 
+// reune em vetor todos elementos do tipo input
 const inputs = document.querySelectorAll('input');
 
+// percorre vetor
 inputs.forEach(input => {
+    // condicional para formatar tipo de input monetário
+    if(input.dataset.tipo === 'preco') {
+        SimpleMaskMoney.setMask(input, {
+            prefix: 'R$',
+            fixed: true,
+            fractionDigits: 2,
+            decimalSeparator: ',',
+            thousandsSeparator: '.',
+            cursor: 'end'
+        })
+    }
+    
+    // executa função valida() em um elemento toda vez que ocorre o evento 'blur'
     input.addEventListener('blur', (evento) => {
         valida(evento.target)
     })

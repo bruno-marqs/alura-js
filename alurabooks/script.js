de6b1506ed22effee834a42fb7eceac02a0042fa
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-05-03 14:39:00 -0300

    criando rotina requisição API viaCEP

---
+++ alurabooks/script.js
@@ -0,0 +1,16 @@
+// atribuindo a variável API viaCEP
+const consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+    // formatando requisição em json (Javascript Object Notation)
+    .then(response => response.json())
+    .then(data => {
+        // condicional para tratar erro
+        if(data.erro) {
+            throw Error('Esse CEP não é válido')
+        } else {
+            console.log(data)
+        }
+    })
+    .catch(erro => console.lot(erro));
+
+// imprimindo resultado da requisição
+console.log(consultaCEP)

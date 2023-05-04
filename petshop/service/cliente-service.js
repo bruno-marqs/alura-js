b7bb8d9547845f5e02357bcc01ea8ff3a78d40ff
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-05-04 11:44:54 -0300

    criando função para inserir template a cada linha de dados

--- petshop/service/cliente-service.js
+++ petshop/service/cliente-service.js
@@ -1,3 +1,19 @@
+// criando função com template para cada linha de cliente
+const criaNovaLinha = (nome, email) => {
+    // atribuindo à variável o template string
+    const conteudo = `
+        <td class="td" data-td>${nome}</td>
+        <td>${email}</td>
+        <td>
+            <ul class="tabela__botoes-controle">
+                <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+            </ul>
+        </td>`
+}
+
+// ------------------------------------------------
+
 //criando objeto de conexão
 const http = new XMLHttpRequest()
 

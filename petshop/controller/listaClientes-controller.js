88fee7bab10e3e3d31d3fa3c0a5396513b1b3ae0
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-05-04 14:53:04 -0300

    separando responsabilidades (service e controller)

---
+++ petshop/controller/listaClientes-controller.js
@@ -0,0 +1,37 @@
+import { listaClientes } from "../service/cliente-service"
+
+// criando função com template para cada linha de cliente
+const criaNovaLinha = (nome, email) => {
+    // criando linha para cada cliente
+    const linhaNovaCliente = document.createElement('tr')
+    // atribuindo à variável o template string
+    const conteudo = `
+        <td class="td" data-td>${nome}</td>
+        <td>${email}</td>
+        <td>
+            <ul class="tabela__botoes-controle">
+                <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+            </ul>
+        </td>`
+    // atribuindo ao elemento html o template
+    linhaNovaCliente.innerHTML = conteudo
+    
+    return linhaNovaCliente
+}
+
+// ------------------------------------------------
+
+// percorrendo árvore do DOM e selecionando elemento por data-attribute
+const tabela = document.querySelector('[data-tabela]')
+
+// ------------------------------------------------
+
+// executando função e manipulando dados
+listaClientes().then( data => {
+    // percorrendo vetor data
+    data.forEach(elemento => {
+        // anexando elemento-filho (child) no elemento-pai (parent)
+        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
+    })
+})
--- petshop/service/cliente-service.js
+++ petshop/service/cliente-service.js
@@ -1,32 +1,6 @@
-// criando função com template para cada linha de cliente
-const criaNovaLinha = (nome, email) => {
-    // criando linha para cada cliente
-    const linhaNovaCliente = document.createElement('tr')
-    // atribuindo à variável o template string
-    const conteudo = `
-        <td class="td" data-td>${nome}</td>
-        <td>${email}</td>
-        <td>
-            <ul class="tabela__botoes-controle">
-                <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-                <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-            </ul>
-        </td>`
-    // atribuindo ao elemento html o template
-    linhaNovaCliente.innerHTML = conteudo
-    
-    return linhaNovaCliente
-}
-
-// ------------------------------------------------
-
-// percorrendo árvore do DOM e selecionando elemento por data-attribute
-const tabela = document.querySelector('[data-tabela]')
-
-// ------------------------------------------------
 
 // função fazendo conexão com API e requisitando dados 
-const listaClientes = () => {
+export const listaClientes = () => {
     // executando fetch
     return fetch(`http://localhost:3000/profile`).then(
         resposta => {
@@ -36,11 +10,3 @@ const listaClientes = () => {
     )
 }
 
-// executando função e manipulando dados
-listaClientes().then( data => {
-    // percorrendo vetor data
-    data.forEach(elemento => {
-        // anexando elemento-filho (child) no elemento-pai (parent)
-        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
-    })
-})

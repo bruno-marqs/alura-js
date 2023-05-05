5f33651fe153bd53d0352e0939437500c162798b
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-05-05 14:17:20 -0300

    função inserir cliente verb POST

--- petshop/service/cliente-service.js
+++ petshop/service/cliente-service.js
@@ -10,7 +10,24 @@ const listaClientes = () => {
     )
 }
 
+// função para inserir dados
+const criaCliente = (nome, email) => {
+    return fetch(`http://localhost:3000/profile`, {
+        // opções da requisição
+        method: 'POST',
+        headers: {
+            'content-type' : 'application/json'
+        },
+        body: JSON.stringify({
+            nome,
+            email
+        })
+    })
+    .then( resposta => { return resposta.body })
+}
+
 // criando objeto para notação
 export const clienteService = {
-    listaClientes
+    listaClientes, 
+    criaCliente
 }

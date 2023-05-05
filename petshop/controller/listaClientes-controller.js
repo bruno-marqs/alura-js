2fb4491ca71ddc557271f20d88a6ae92427885e7
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-05-05 15:15:48 -0300

    criando eventoListener para removerCliente + add id ao dataset de cada linha criada

--- petshop/controller/listaClientes-controller.js
+++ petshop/controller/listaClientes-controller.js
@@ -1,7 +1,7 @@
 import { clienteService } from "../service/cliente-service.js"
 
 // criando função com template para cada linha de cliente
-const criaNovaLinha = (nome, email) => {
+const criaNovaLinha = (nome, email, id) => {
     // criando linha para cada cliente
     const linhaNovaCliente = document.createElement('tr')
     // atribuindo à variável o template string
@@ -16,6 +16,7 @@ const criaNovaLinha = (nome, email) => {
         </td>`
     // atribuindo ao elemento html o template
     linhaNovaCliente.innerHTML = conteudo
+    linhaNovaCliente.dataset.id = id
     
     return linhaNovaCliente
 }
@@ -25,6 +26,15 @@ const criaNovaLinha = (nome, email) => {
 // percorrendo árvore do DOM e selecionando elemento por data-attribute
 const tabela = document.querySelector('[data-tabela]')
 
+// criando eventListener
+tabela.addEventListener('click', (evento) => {
+    // selecionando botao excluir pela class que ocorreu evento
+    let botaoDeletar = evento.target.className == 'botao-simples--excluir';
+    if(botaoDeletar){
+        clienteService.removeCliente(id)
+    }
+})
+
 // ------------------------------------------------
 
 // executando função e manipulando dados
@@ -32,6 +42,6 @@ clienteService.listaClientes().then( data => {
     // percorrendo vetor data
     data.forEach(elemento => {
         // anexando elemento-filho (child) no elemento-pai (parent)
-        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
+        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email, elemento.id))
     })
 })

880bad7c5de81018b3667ca3a5c5c8d55d8fd4a4
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-05-05 15:55:21 -0300

    requisita dados por id pela função e atribui valor as variaveis selecionadas por data-attribute

--- petshop/controller/atualizaCliente-controller.js
+++ petshop/controller/atualizaCliente-controller.js
@@ -1,5 +1,20 @@
+import { clienteService } from "../service/cliente-service.js"
+
 // capturando url do cliente selecionado
 const pegaUrl = new URL(window.location)
 
 // atribuindo variavel valor do id da URL
 const id = pegaUrl.searchParams.get('id')
+
+// ---------------------------------------------------------------
+
+// percorre árvore DOM e atribui a variavel
+const inputNome = document.querySelector('[data-nome]')
+const inputEmail = document.querySelector('[data-email]')
+
+// executa função e atribui dados encontrados por id às variáveis
+clienteService.detalhaCliente(id)
+.then( dados => {
+    inputNome.value = dados.nome
+    inputEmail.value = dados.email
+})
